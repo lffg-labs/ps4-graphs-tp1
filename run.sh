9fc1cf1aabecbf7aac3b91637d2b4015ba546c73
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2023-11-04 19:22:54 -0300

    add run.sh helper

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+algos=("atofigh" "gabow")
+target="//apps:arbok_cli"
+
+[ $# -eq 0 ] && echo "error: missing argument with input file" && exit 1
+input="$(realpath "$1")"
+echo "input is ($input)"
+
+tmp_dir="${PS4_GRAPHS_OUT_DIR:-$(mktemp -d)}"
+echo "tmp dir is ($tmp_dir)"
+
+echo "building cli..."
+bazel build "$target" || exit 1
+
+program="$(bazel cquery --output=files "$target" 2>/dev/null)"
+
+for algo in "${algos[@]}"; do
+    out_file="$tmp_dir/$algo.out"
+
+    echo "running ($algo)..."
+    set -x
+    "$program" -algo "$algo" -input "$input" -outfile "$out_file"
+    status="$?"
+    set +x
+
+    [ $status -ne 0 ] && echo "error: while running $algo" && exit 1
+    sort -o "$out_file" "$out_file"
+    echo "done"
+done
